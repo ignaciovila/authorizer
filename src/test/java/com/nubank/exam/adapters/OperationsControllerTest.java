e189b7931b20ed57a8f15e7c40f214762e8a60e3
Vila <ignacio.vilagraca@mercadolibre.com>
2021-12-06 11:37:51 -0300

    simplify path

--- src/test/java/com/nubank/exam/adapters/OperationsControllerTest.java
+++ src/test/java/com/nubank/exam/adapters/OperationsControllerTest.java
@@ -28,56 +28,56 @@ class OperationsControllerTest {
 
     @Test
     void accountCreationSuccess() throws IOException, JSONException {
-        baseTest("./src/test/resources/input/account_creation_success.json",
-                "./src/test/resources/expected/account_creation_success_response.json");
+        baseTest("src/test/resources/input/account_creation_success.json",
+                "src/test/resources/expected/account_creation_success_response.json");
     }
 
     @Test
     void accountCreationError() throws IOException, JSONException {
-        baseTest("./src/test/resources/input/account_creation_error.json",
-                "./src/test/resources/expected/account_creation_error_response.json");
+        baseTest("src/test/resources/input/account_creation_error.json",
+                "src/test/resources/expected/account_creation_error_response.json");
     }
 
     @Test
     void processTransactionSuccess() throws IOException, JSONException {
-        baseTest("./src/test/resources/input/process_transaction_success.json",
-                "./src/test/resources/expected/process_transaction_success_response.json");
+        baseTest("src/test/resources/input/process_transaction_success.json",
+                "src/test/resources/expected/process_transaction_success_response.json");
     }
 
     @Test
     void processTransactionAccountNotInitializedError() throws IOException, JSONException {
-        baseTest("./src/test/resources/input/process_transaction_account_not_initialized_error.json",
-                "./src/test/resources/expected/process_transaction_account_not_initialized_error_response.json");
+        baseTest("src/test/resources/input/process_transaction_account_not_initialized_error.json",
+                "src/test/resources/expected/process_transaction_account_not_initialized_error_response.json");
     }
 
     @Test
     void processTransactionCardNotActiveError() throws IOException, JSONException {
-        baseTest("./src/test/resources/input/process_transaction_card_not_active_error.json",
-                "./src/test/resources/expected/process_transaction_card_not_active_error_response.json");
+        baseTest("src/test/resources/input/process_transaction_card_not_active_error.json",
+                "src/test/resources/expected/process_transaction_card_not_active_error_response.json");
     }
 
     @Test
     void processTransactionInsufficientLimitError() throws IOException, JSONException {
-        baseTest("./src/test/resources/input/process_transaction_insufficient_limit_error.json",
-                "./src/test/resources/expected/process_transaction_insufficient_limit_error_response.json");
+        baseTest("src/test/resources/input/process_transaction_insufficient_limit_error.json",
+                "src/test/resources/expected/process_transaction_insufficient_limit_error_response.json");
     }
 
     @Test
     void processTransactionHighFrequencySmallIntervalError() throws IOException, JSONException {
-        baseTest("./src/test/resources/input/process_transaction_high_frequency_small_interval_error.json",
-                "./src/test/resources/expected/process_transaction_high_frequency_small_interval_error_response.json");
+        baseTest("src/test/resources/input/process_transaction_high_frequency_small_interval_error.json",
+                "src/test/resources/expected/process_transaction_high_frequency_small_interval_error_response.json");
     }
 
     @Test
     void processTransactionDoubledTransactionError() throws IOException, JSONException {
-        baseTest("./src/test/resources/input/process_transaction_doubled_transaction_error.json",
-                "./src/test/resources/expected/process_transaction_doubled_transaction_error_response.json");
+        baseTest("src/test/resources/input/process_transaction_doubled_transaction_error.json",
+                "src/test/resources/expected/process_transaction_doubled_transaction_error_response.json");
     }
 
     @Test
     void processTransactionMultipleErrors() throws IOException, JSONException {
-        baseTest("./src/test/resources/input/process_transaction_multiple_errors.json",
-                "./src/test/resources/expected/process_transaction_multiple_errors_response.json");
+        baseTest("src/test/resources/input/process_transaction_multiple_errors.json",
+                "src/test/resources/expected/process_transaction_multiple_errors_response.json");
     }
 
     private void baseTest(String inputRoute, String expectedRoute) throws IOException, JSONException {
